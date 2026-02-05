2cc7301b937e6e449145946f6c6321abd76cce4a
baeley <baeleycanning@hotmail.com>
2026-02-06 02:30:19 +1300

    Track

---
+++ dist/_astro/hoisted.CZpH73f7.js
@@ -0,0 +1,1 @@
+import"./hoisted.DJx6xKnv.js";window&&window.dataLayer&&window.dataLayer.push({event:"lead_submit",source:"thanks_page"});
--- dist/_astro/hoisted.DgjIvOGc.js
+++
@@ -1,1 +0,0 @@
-import"./hoisted.Bbf1S7kI.js";window&&window.dataLayer&&window.dataLayer.push({event:"lead_submit",source:"thanks_page"});
