f081da731e1d73c926633f524ad18cdc4950a1a5
baeley <baeleycanning@hotmail.com>
2026-02-12 14:39:43 +1300

    Swap

---
+++ dist/_astro/hoisted.CKkL58DC.js
@@ -0,0 +1,1 @@
+import"./hoisted.dE2p-F4p.js";window&&window.dataLayer&&window.dataLayer.push({event:"lead_submit",source:"thanks_page"});
--- dist/_astro/hoisted.CZpH73f7.js
+++
@@ -1,1 +0,0 @@
-import"./hoisted.DJx6xKnv.js";window&&window.dataLayer&&window.dataLayer.push({event:"lead_submit",source:"thanks_page"});
